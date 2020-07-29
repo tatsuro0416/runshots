1385a08fc76c9f2089ec056930dd1d38c2686bfb
Tatsuro Suzuki <tatsuro.s0416@gmail.com>
2020-07-29 09:06:31 +0900

    #10 ユーザー情報変更機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,17 @@ class User < ApplicationRecord
       self.profile_image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-icon.png')), filename: 'default-icon.png', content_type: 'image/png')
     end
   end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, # ここの行にカンマを追加
+    controllers: { registrations: 'registrations' }
   
   resources :users, only: %i(show) do
   end
