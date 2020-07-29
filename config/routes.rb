6131aeeebd26f99fa3b00b9ce102ba0311fae13e
Tatsuro Suzuki <tatsuro.s0416@gmail.com>
2020-07-29 09:09:13 +0900

    #10 コメント修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users, # ここの行にカンマを追加
+  devise_for :users,
     controllers: { registrations: 'registrations' }
   
   resources :users, only: %i(show) do
   end
-
 
   resources :posts, only: %i(new create show edit delete) do
     resources :photos, only: %i(create)
